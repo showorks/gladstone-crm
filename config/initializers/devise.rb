8bfec68e312642c47b6f39c0d46dd24c37a6aaa7
Terrell Miller <terrell@terrellmiller.com>
2015-04-05 14:20:34 -0500

    Set devise remember_for to 2 weeks

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -126,7 +126,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
-  # config.remember_for = 2.weeks
+  config.remember_for = 2.weeks
 
   # Invalidates all the remember me tokens when the user signs out.
   config.expire_all_remember_me_on_sign_out = true
@@ -153,7 +153,7 @@ Devise.setup do |config|
   # config.timeout_in = 30.minutes
 
   # If true, expires auth token on session timeout.
-  config.expire_auth_token_on_timeout = true
+  # config.expire_auth_token_on_timeout = false
 
   # ==> Configuration for :lockable
   # Defines which strategy will be used to lock an account.
